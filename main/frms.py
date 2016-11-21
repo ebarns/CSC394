f5125ec292fb07965265eafb21dc0f81b8142549
ebarns <erik.barns@yahoo.com>
2016-11-21 04:11:59 +0000

    account updates and fixes

--- main/frms.py
+++ main/frms.py
@@ -15,8 +15,6 @@ class AccountForm(forms.Form):
     usrname = forms.CharField(label="Username", required=True)
     email   = forms.EmailField(label="Email", required=True)
     mjr     = forms.ChoiceField(label="Major", choices=[(3,'MS CS: Software and Systems Development'),(4,'MS IS: Business Analysis/Systems Analysis'), (5,'MS IS: Standard')],required=True)
-    enrled  = forms.BooleanField(required=False, label="Enrolled")
-    fclty   = forms.BooleanField(required=False, label="Faculty")
     
     def save(self, usr, commit=True):
         user = usr
@@ -24,8 +22,6 @@ class AccountForm(forms.Form):
         user.last_name   = self.cleaned_data['last']
         user.email  = self.cleaned_data['email']
         user.degree = self.cleaned_data['mjr']
-        user.isEnrolled = self.cleaned_data['enrled']
-        user.isFaculty  = self.cleaned_data['fclty']
         
         if commit:
             user.save()
--- main/urls.py
+++ main/urls.py
@@ -16,6 +16,9 @@ urlpatterns = [
     url(r'^account',views.account, name="account"),
     url(r'^addClass',views.addClass, name="addClass"),
     url(r'^removePlan',views.removePlan, name="removePlan"),
-    url(r'^view_student/(?P<username>\w+)',views.view_student,name='student view'),
-    url(r'^view_path/(?P<pth_id>\w+)',views.view_path ,name='path view'),
+    url(r'^view_path/([0-9]+)',views.view_path ,name='path view'),
+    url(r'^view_student/(?P<username>\w+)/$',views.view_student,name='student view'),
+    url(r'^view_student/(\w+)/view_path/(?P<pth_id>\w+)',views.view_path,name='student plan view'),
+    
+    
 ]
